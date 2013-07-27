c321099878e47a8c1e78e67f0fadbe37a85ad4fa
James Wucher <james@nonlinearideas.com>
2013-07-26 19:22:59 -0500

    Initial Commit

---
+++ spine_test/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  spine_testAppController.h
+//  spine_test
+//
+//  Created by James Wucher on 7/26/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ spine_test/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  spine_testAppController.h
+//  spine_test
+//
+//  Created by James Wucher on 7/26/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
